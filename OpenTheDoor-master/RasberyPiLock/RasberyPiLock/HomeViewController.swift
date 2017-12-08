edc80535eca6a8868e8555234502318e4877be64
MMikkilineni <mmikkilineni6@gmail.com>
2017-12-08 02:15:23 -0800

    siri

--- OpenTheDoor-master/RasberyPiLock/RasberyPiLock/HomeViewController.swift
+++ OpenTheDoor-master/RasberyPiLock/RasberyPiLock/HomeViewController.swift
@@ -10,12 +10,21 @@
 import Foundation
 import Intents
 import UIKit
+import Speech
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, SFSpeechRecognizerDelegate {
     
     @IBOutlet weak var visitorimage: UIImageView!
     @IBOutlet weak var visitorname: UILabel!
    // @IBOutlet weak var addbutton: UIButton!
+    //SpeechFramework
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var microphoneButton: UIButton!
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))!
     
     var notknown = ["unknown", "not known", "not found"]
 
@@ -58,6 +67,37 @@ class HomeViewController: UIViewController {
         
         self.whoIsAtTheDoor()
         self.fetchPhoto()
+        
+        //SpeechKit
+        microphoneButton.isEnabled = false
+        
+        speechRecognizer.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -76,6 +116,21 @@ class HomeViewController: UIViewController {
     }
     
     
+    @IBAction func microphoneTapped(_ sender: Any) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+            microphoneButton.setImage( #imageLiteral(resourceName: "mic"), for: UIControlState.normal)
+            
+        } else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
+            microphoneButton.setImage( #imageLiteral(resourceName: "micRecording"), for: UIControlState.normal)
+            
+        }
+    }
     
     func whoIsAtTheDoor() {
         let req = NSMutableURLRequest(url: NSURL(string:"http://192.168.0.12:5000/clickPhoto")! as URL)
@@ -156,6 +211,115 @@ class HomeViewController: UIViewController {
                 print(String(data: data!, encoding: String.Encoding.utf8) ?? "default error ")
             }
             }.resume()
+    }
+    
+    //SpeechKit
+    /*
+     * Start Recorder
+     */
+    func startRecording() {
+        if recognitionTask != nil {  //1
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()  //2
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()  //3
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }  //4
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        } //5
+        
+        recognitionRequest.shouldReportPartialResults = true  //6
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in  //7
+            
+            var isFinal = false  //8
+            //Open & Close the door using RaspberryPiLock
+            if(result?.bestTranscription.formattedString == "Open the door using RasberyPiLock"){
+                let req = NSMutableURLRequest(url: NSURL(string:"http://192.168.0.20:5000/unlock")! as URL)
+                req.httpMethod = "GET"
+                req.httpBody = "key=\"value\"".data(using: String.Encoding.utf8)
+                URLSession.shared.dataTask(with: req as URLRequest) { data, response, error in
+                    if error != nil {
+                        // Error
+                        print(error?.localizedDescription ?? "default error ")
+                    } else {
+                        // Successfull
+                        print(String(data: data!, encoding: String.Encoding.utf8) ?? "default error ")
+                    }
+                    }.resume()
+            }else if(result?.bestTranscription.formattedString == "Close the door using RasberyPiLock"){
+                let req = NSMutableURLRequest(url: NSURL(string:"http://192.168.0.12:5000/lock")! as URL)
+                req.httpMethod = "GET"
+                req.httpBody = "key=\"value\"".data(using: String.Encoding.utf8)
+                URLSession.shared.dataTask(with: req as URLRequest) { data, response, error in
+                    if error != nil {
+                        // Error
+                        print(error?.localizedDescription ?? "default error ")
+                    } else {
+                        // Successfull
+                        print(String(data: data!, encoding: String.Encoding.utf8) ?? "default error ")
+                    }
+                    }.resume()
+            }
+            if result != nil {
+                
+                self.textView.text = result?.bestTranscription.formattedString  //9
+                //self.store?.setObject( self.textView.text as CKRecordValue?, forKey: "VoiceText")
+                //self.storeInCloud()
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {  //10
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.microphoneButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)  //11
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()  //12
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        textView.text = "Say something, I'm listening!"
+        
+    }
+    
+    /*
+     * speech Recognizer
+     */
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            microphoneButton.isEnabled = true
+        } else {
+            microphoneButton.isEnabled = false
+        }
     }
 }
 
